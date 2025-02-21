7c393a579c9a9364a311b5a7d577edaa11c505a1
Johann Haslinger <johann.hsr@icloud.com>
2025-02-21 19:13:12 +0100

    skibidi fortnite player

---
+++ Assets/02_Scripts/02_PlayerController/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce = 5f; // Höhe des Sprungs
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
