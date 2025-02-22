49ccb7ca21a4fd578e72c3505b4b8277f4c6cf0f
Johann Haslinger <johann.hsr@icloud.com>
2025-02-22 21:33:08 +0100

    add bg

--- Assets/02_Scripts/06_Shop/ShopItemUI.cs
+++ Assets/02_Scripts/06_Shop/ShopItemUI.cs
@@ -4,21 +4,34 @@ using UnityEngine.UI;
 
 public class ShopItemUI : MonoBehaviour
 {
-    // public TextMeshProUGUI itemNameText;
-    public TextMeshProUGUI priceText;
     public Image itemImage;
     public Button buyButton;
+    public Outline itemOutline;
 
     private ShopItem currentItem;
 
     public void Setup(ShopItem item)
     {
         currentItem = item;
-        // itemNameText.text = item.itemName;
-        priceText.text = item.price + " Coins";
         itemImage.sprite = item.itemImage;
 
         buyButton.onClick.RemoveAllListeners();
         buyButton.onClick.AddListener(() => ShopManager.instance.BuyItem(currentItem));
+
+        UpdateItemUI();
+    }
+
+    private void UpdateItemUI()
+    {
+        if (CoinManager.instance.score >= currentItem.price)
+        {
+
+            itemImage.color = new Color(itemImage.color.r, itemImage.color.g, itemImage.color.b, 1f);
+        }
+        else
+        {
+
+            itemImage.color = new Color(itemImage.color.r, itemImage.color.g, itemImage.color.b, 0.5f);
+        }
     }
 }
