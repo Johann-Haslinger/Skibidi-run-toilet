528307e5e743f848fc22456e3cc36eb36f1eaffe
JeldrikGaal <jeldrikgaal@gmail.com>
2025-02-22 23:04:28 +0100

    game flow complete

--- Assets/02_Scripts/00_Tools/SceneLoader.cs
+++ Assets/02_Scripts/00_Tools/SceneLoader.cs
@@ -4,6 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private bool _loadOnStart = false;
+    [SerializeField] private int _loadOnStartNum;
+
+    private void Start()
+    {
+        if (_loadOnStart)
+        {
+            var blackOut = FindFirstObjectByType<BlackOutScreen>();
+            if (blackOut)
+            {
+                blackOut.Blackout(1);
+            }
+            LoadSceneByIndexWithDelay(_loadOnStartNum);
+        }
+    }
+
     public void LoadSceneByIndex(int num)
     {
         SceneManager.LoadScene(num);
