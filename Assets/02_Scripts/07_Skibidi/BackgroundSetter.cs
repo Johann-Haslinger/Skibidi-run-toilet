45815d07d192fbee3b229a18ee4b1275570c11e1
Johann Haslinger <johann.hsr@icloud.com>
2025-02-22 18:25:02 +0100

    feat: add background

---
+++ Assets/02_Scripts/07_Skibidi/BackgroundSetter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BackgroundSetter : MonoBehaviour
+{
+    public Sprite backgroundSprite; // Hier das Bild im Inspector zuweisen
+
+    void Start()
+    {
+        // Neues GameObject für den Hintergrund erstellen
+        GameObject bg = new GameObject("Background");
+        
+        // SpriteRenderer hinzufügen
+        SpriteRenderer sr = bg.AddComponent<SpriteRenderer>();
+        sr.sprite = backgroundSprite;
+        
+        // Hintergrund hinter allem anzeigen (höhere negative Z-Position)
+        bg.transform.position = new Vector3(0, 0, 10);
+        
+        // Kamera-Breite und Höhe berechnen
+        float screenHeight = Camera.main.orthographicSize * 2f;
+        float screenWidth = screenHeight * Screen.width / Screen.height;
+        
+        // Größe des Sprites anpassen
+        if (sr.sprite != null)
+        {
+            float spriteWidth = sr.sprite.bounds.size.x;
+            float spriteHeight = sr.sprite.bounds.size.y;
+            bg.transform.localScale = new Vector3(screenWidth / spriteWidth, screenHeight / spriteHeight, 1);
+        }
+    }
+}
